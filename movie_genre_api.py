80a2f7fde7148ec728104281d09d855742306dab
Leonardo Guzman <hleonardo.guzmano@gmail.com>
2025-05-25 15:15:51 -0500

    Update movie_genre_api.py

--- movie_genre_api.py
+++ movie_genre_api.py
@@ -106,4 +106,4 @@ class MovieGenreApi(Resource):
 
 # Ejecutar aplicación
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5001)
